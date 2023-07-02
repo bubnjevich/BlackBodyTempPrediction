4813cbf715e03fe54e372506bf994d692950047e
Veljko Bubnjevic <veljkobubnjevic01@gmail.com>
2023-07-02 18:04:50 +0200

    added cnn

--- cnn.py
+++ cnn.py
@@ -5,7 +5,7 @@ from tensorflow.keras.layers import Conv1D, Dense, Flatten, InputLayer, MaxPooli
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
 from keras.models import load_model
-
+import pickle
 
 h = 6.626e-34
 c = 3.0e+8
@@ -22,12 +22,9 @@ wavelengths = np.arange(1e-9, 3e-6, 1e-9)
 
 
 def generate(scaler_X, scaler_Y):
-    # Generisanje talasnih dužina u opsegu od 400 nm do 700 nm
     
-    # Generisanje temperatura u opsegu od 3000 K do 8000 K
     temperatures = np.linspace(3000, 8000, 100)
     
-    # Kreiranje X_train i Y_train
     X_train = []
     Y_train = []
     for temperature in temperatures:
@@ -39,14 +36,11 @@ def generate(scaler_X, scaler_Y):
     Y_train = np.array(Y_train)
     
     
-    # Skaliranje podataka
     scaler_X.fit(X_train)
     scaler_Y.fit(Y_train)
     X_train_scaled = scaler_X.transform(X_train)
     
-    # Inicijalizacija MinMaxScaler
     
-    # Skaliranje podataka
     Y_train_scaled = scaler_Y.transform(Y_train)
     
     return CNN(X_train_scaled, Y_train_scaled)
@@ -62,49 +56,54 @@ def CNN(X_train_scaled, Y_train_scaled):
 
     model.compile(loss='mse', optimizer='adam')
     model.fit(X_train_scaled, Y_train_scaled, epochs=50, batch_size=16)
-    # model.save('planck.h5')
+    model.save(f'cnnModel/planck.h5')
+    
     return model
 
 
-def prediction(model, X_test_scaled):
-    Y_pred_scaled = model.predict(X_test_scaled)
-    return Y_pred_scaled
+
+def cnn_prediction(model_file, file):
+    
+    model = load_model(model_file)
+    
+    temperature_str = file.split('/')[-1]
+    temperature_str = temperature_str.rstrip('.txt')
+    T = float(temperature_str[1:-1])
+    
+    X_test_scaled = np.loadtxt(file)
+    X_test_scaled = X_test_scaled.reshape(1, -1)
+    
+    prediction = model.predict(X_test_scaled)
+    
+    new_intensity = plank_law(wavelengths, T)
+    plt.clf()
+    plt.plot(wavelengths*1e9, new_intensity, color='red', label='T={}'.format(T))
+    
+    with open(f'scalers/scaler_Y.pkl', 'rb') as f:
+        scalerY = pickle.load(f)
+
+        temperature = scalerY.inverse_transform(prediction)[0][0]
+        predicted_intensity = plank_law(wavelengths, temperature)
+        plt.plot(wavelengths*1e9, predicted_intensity, color='black', label='T={}'.format(temperature))
+    
+        plt.title("Planck's law of radiation")
+        plt.xlabel("Wavelength [nm]")
+        plt.ylabel("Intensity [W/(m^2*nm)]")
+        plt.legend()
+        plt.savefig(f'resultsCNN/{temperature:.2f}K.png')
+        f.close()
+        return temperature, T
 
 
 if __name__ == '__main__':
    scaler_X = StandardScaler()
    scaler_Y = StandardScaler()
    model = generate(scaler_X, scaler_Y)
-
-   # izračunajte intenzitete na novim talasnim dužinama
    
-   T = [3000, 4000, 6000]
-   for t in T:
-       new_intensity = plank_law(wavelengths, t)
-       plt.plot(wavelengths*1e9, new_intensity, color='red', label='T={}'.format(t))
-       
-       X_test = [new_intensity.flatten()]
-       X_test = np.array(X_test)
-       X_test = scaler_X.transform(X_test.reshape(1, -1))
-       # izvršite predikciju
-       prediction = prediction(model, X_test)
-       
-       # transformišite predikciju nazad u originalni oblik
-       temperature = scaler_Y.inverse_transform(prediction)[0][0]
-       predicted_intensity = plank_law(wavelengths, temperature)
-       plt.plot(wavelengths*1e9, predicted_intensity, color='black', label='T={}'.format(temperature))
-    
-       print(temperature)
-       
-       
-   plt.title("Plankov zakon zračenja")
-   plt.xlabel("Talasna dužina [nm]")
-   plt.ylabel("Intenzitet [W/(m^2*nm)]")
-   x_min, x_max = plt.xlim()
-   y_min, y_max = plt.ylim()
-   print('Granice x osa: [{}, {}]'.format(x_min, x_max))
-   print('Granice y osa: [{}, {}]'.format(y_min, y_max))
-   # plt.legend()
-   plt.show()
+   with open(f'scalers/scaler_X.pkl', 'wb') as f:
+       pickle.dump(scaler_X, f)
+   
+   with open(f'scalers/scaler_Y.pkl', 'wb') as f:
+       pickle.dump(scaler_Y, f)
 
     
